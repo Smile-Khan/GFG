cd97783065526a7f509052bd32c4e570e115ae83
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2023-10-15 15:04:34 +0530

    Create Normal BST to Balanced BST.java

---
+++ EASY/Normal BST to Balanced BST.java
@@ -0,0 +1,214 @@
+QUESTION:- Normal BST to Balanced BST 
+
+https://practice.geeksforgeeks.org/problems/normal-bst-to-balanced-bst/1
+
+
+SOLUTION :-
+
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Node
+{
+    int data;
+    Node right, left;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinarySearchTree
+{
+     static Node insert(Node node, int data)
+    {
+     if (node == null)
+     {
+            return (new Node(data));
+    } else 
+        {
+             
+            /* 2. Otherwise, recur down the tree */
+            if (data <= node.data)
+            {
+                node.left = insert(node.left, data);
+            } else 
+            {
+                node.right = insert(node.right, data);
+            }
+ 
+            /* return the (unchanged) node pointer */
+            return node;
+        }
+    
+    }
+    
+    int height(Node node) 
+{
+   if (node==null) return 0;
+   else
+   {
+       int lDepth = height(node.left);
+       int rDepth = height(node.right);
+       if (lDepth > rDepth)
+           return(lDepth+1);
+       else 
+           return(rDepth+1);
+   }
+} 
+    static Node buildTree(String str)
+    {
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+         public static void main (String[] args) throws IOException
+    {
+       
+        
+       
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            
+            int t=Integer.parseInt(br.readLine());
+            
+            while(t > 0){
+                String s = br.readLine();
+                Node root = buildTree(s);
+                
+           
+             GfG gfg = new GfG();
+             
+          root =  gfg.buildBalancedTree(root);
+            // preOrder(root);
+            BinarySearchTree bst = new BinarySearchTree();
+             System.out.println(bst.height(root));
+             t--;
+        }
+    }
+    
+     void preOrder(Node node) 
+    {
+        if (node == null)
+            return;
+        System.out.print(node.data + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+}
+
+
+// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node right, left;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+
+class GfG
+{
+    // List to store the in-order traversal of the tree
+    List<Node> inOrderList = new ArrayList<>();
+    
+    // Helper function to do an in-order traversal of the tree
+    void inOrderTraversal(Node root) {
+        if (root == null)
+            return;
+        inOrderTraversal(root.left);
+        inOrderList.add(root);
+        inOrderTraversal(root.right);
+    }
+
+    // Function to convert a sorted array into a balanced BST
+    Node sortedArrayToBST(List<Node> list, int start, int end) {
+        if (start > end)
+            return null;
+
+        // Find the middle element
+        int mid = (start + end) / 2;
+        
+        // Construct the root node
+        Node root = list.get(mid);
+
+        // Recursively construct the left and right subtrees
+        root.left = sortedArrayToBST(list, start, mid - 1);
+        root.right = sortedArrayToBST(list, mid + 1, end);
+
+        return root;
+    }
+
+    Node buildBalancedTree(Node root) 
+    {
+        // Perform an in-order traversal of the tree to get sorted elements
+        inOrderTraversal(root);
+
+        // Convert the in-order list into a balanced BST
+        int n = inOrderList.size();
+        return sortedArrayToBST(inOrderList, 0, n - 1);
+        
+    }
+}
