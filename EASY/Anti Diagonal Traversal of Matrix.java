94af242fb76adf7a308cb7d51b9f1de4f31367a2
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2023-12-27 23:09:07 +0530

    Create Anti Diagonal Traversal of Matrix.java

---
+++ EASY/Anti Diagonal Traversal of Matrix.java
@@ -0,0 +1,66 @@
+public class Anti Diagonal Traversal of Matrix {
+    https://www.geeksforgeeks.org/problems/print-diagonally1623/1
+
+}
+
+
+SOLUTION :-
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            
+            String input_line[] = br.readLine().trim().split("\\s+");
+            int[][] matrix = new int[n][n];
+            for(int i=0; i<n; i++) {
+                for (int j = 0; j < n; ++j) {
+                    matrix[i][j] = Integer.parseInt(input_line[i * n + j]); 
+                }
+            }
+            Solution obj = new Solution();
+            int[] ans = obj.antiDiagonalPattern(matrix);
+            for(int i = 0; i < ans.length; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int[] antiDiagonalPattern(int[][] matrix)
+    {
+        // Code here
+           int n=matrix.length;
+        int arr[]=new int[n*n];
+        int idx=-1;
+        for(int i=0;i<n;i++){
+            for(int r=0, c=i;r<=i;r++,c--){
+                arr[++idx]=matrix[r][c];
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int r=i,c=n-1;r<n;r++,c--){
+                arr[++idx]=matrix[r][c];
+            }
+        }
+        return arr;
+
+    }
+}
