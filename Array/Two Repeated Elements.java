848ed79915b4f1007fa081cd8250e91f86add6ce
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2024-04-18 23:07:23 +0530

    Create Two Repeated Elements.java

---
+++ Array/Two Repeated Elements.java
@@ -0,0 +1,65 @@
+QUESTION :-  https://www.geeksforgeeks.org/problems/two-repeated-elements-1587115621/1
+
+
+SOLUTION :- 
+
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class GFG
+ {
+	public static void main (String[] args) throws IOException
+	 {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int t = Integer.parseInt(br.readLine());
+	    for(int i=0;i<t;i++){
+	        int n = Integer.parseInt(br.readLine());
+	        String l = br.readLine();
+    		String[] sarr = l.split(" ");
+    		int[] arr = new int[sarr.length];
+    		for(int i1=0;i1<arr.length;i1++){
+    			arr[i1] = Integer.parseInt(sarr[i1]);
+    		}
+    		
+            Solution obj = new Solution();
+            
+            int[] res = obj.twoRepeated(arr, n);
+            System.out.println(res[0] + " " + res[1]);
+	    }
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find two repeated elements.
+    public int[] twoRepeated(int arr[], int n)
+    {
+        // Your code here
+        int a = -1;
+        int b = -1;
+        
+        for(int i : arr)
+        {
+            int id = Math.abs(i);
+            
+            if(arr[id] < 0)
+            {
+                if(a == -1) 
+                a = id;
+                else 
+                b = id;
+            }
+            else arr[id] = -arr[id];
+        }
+        return new int[]{a,b};
+    }
+    
+}
