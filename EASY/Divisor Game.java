9fa801ea9c5df2206cad7bb971bc4ebe6024f96b
Pathan Ismailkhan <129515476+Smile-Khan@users.noreply.github.com>
2024-05-09 17:09:19 +0530

    Create Divisor Game.java

---
+++ EASY/Divisor Game.java
@@ -0,0 +1,39 @@
+public class Divisor Game {
+    
+    https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+
+}
+
+SOLUTION :-
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            Solution obj = new Solution();
+            String ans = obj.divisorGame(n) ? "True" : "False";
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public static boolean divisorGame(int n) {
+        // code here
+        return n % 2 == 0;
+    }
+}
+
